07ec092026b398a6720968620e246affdc30f1f4
Anton <antonliann@gmail.com>
2021-02-14 18:52:35 +0200

    Moves managing and jumping

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
 function Square(props) {
     return (
         <button
             className="square"
             onClick={props.onClick}
+            key={props.keyName}
         >
             {props.value}
         </button>
@@ -17,6 +17,7 @@ function Square(props) {
 class Board extends React.Component {
     renderSquare(i) {
         return (<Square
+            keyName={'sq' + i}
             value={this.props.squares[i]}
             onClick={() => this.props.onClick(i)}
         />);
@@ -24,14 +25,32 @@ class Board extends React.Component {
 
     render() {
         let result = [];
-        for (let row = 0; row < 3; row++){
+        for (let row = 0; row < 3; row++) {
             let thisRow = [];
             for (let col = 0; col < 3; col++) {
-                thisRow.push(this.renderSquare(row*3 + col));
+                thisRow.push(this.renderSquare(row * 3 + col));
             }
             result.push(<div className="board-row"> {thisRow} </div>)
         }
         return result;
+    }
+}
+
+class LeftBoard extends React.Component {
+    render() {
+        return (
+            <div className="left">
+                <button className='new-game' onClick={this.props.newGame}>
+                    <svg className='refresh' width="26" height="26" viewBox="0 0 26 26" fill="none"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M24.4055 11.9641C23.8316 11.9641 23.3695 12.4262 23.3695 13C23.3695 18.718 18.718 23.3695 13 23.3695C7.28203 23.3695 2.63047 18.718 2.63047 13C2.63047 7.28203 7.28203 2.63047 13 2.63047C16.123 2.63047 19.0176 4.00664 20.9777 6.37305H17.3977C16.8238 6.37305 16.3617 6.83516 16.3617 7.40898C16.3617 7.98281 16.8238 8.44492 17.3977 8.44492H23.2172C23.791 8.44492 24.2531 7.98281 24.2531 7.40898V1.59453C24.2531 1.0207 23.791 0.558594 23.2172 0.558594C22.6434 0.558594 22.1812 1.0207 22.1812 1.59453V4.59063C19.8402 2.03633 16.5445 0.558594 13 0.558594C6.13945 0.558594 0.558594 6.13945 0.558594 13C0.558594 19.8605 6.13945 25.4414 13 25.4414C19.8605 25.4414 25.4414 19.8605 25.4414 13C25.4414 12.4262 24.9793 11.9641 24.4055 11.9641Z"
+                            fill="white"/>
+                    </svg>
+                    New game
+                </button>
+            </div>
+        )
     }
 }
 
@@ -68,6 +87,12 @@ class Game extends React.Component {
             stepNumber: step,
             xIsNext: (step % 2) === 0,
         });
+        if (!step) this.setState({
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+        });
+
     }
 
     render() {
@@ -75,24 +100,28 @@ class Game extends React.Component {
         const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
 
-        const moves = history.map((step, move) => {
-            const desc = move ?
-                'Move #' + move :
-                'Go to the game start';
-            return (
-                <li key={move}>
-                    <button onClick={() => this.jumpTo(move)}>
-                        {desc}
-                    </button>
-                </li>
-            );
-        });
+        const moves = history.slice(1).map(
+            (step, move) => {
+                const desc = 'Move #' + ++move;
+                if (this.state.stepNumber !== move)
+                    return (
+                        <button key={move} className='move-button' onClick={() => this.jumpTo(move)}>
+                            {desc}
+                        </button>
+                    )
+                else
+                    return (
+                        <button key={move} className='move-button selected'>
+                            {desc}
+                        </button>
+                    );
+            });
 
         let status;
         status = winner ?
             'The winner is ' + winner :
-            history.length <  10 ?
-                'Next player: ' + (this.state.xIsNext ? 'X' : 'O') :
+            this.state.stepNumber < 10 ?
+                'Current player: ' + (this.state.xIsNext ? 'X' : 'O') :
                 'Draw';
 
         return (
@@ -103,24 +132,28 @@ class Game extends React.Component {
                         onClick={(i) => this.handleClick(i)}
                     />
                 </div>
+                <LeftBoard
+                    newGame={() => this.jumpTo(0)}
+                />
+                <div className="right">
+                    <h2>Go to the move:</h2>
+                    {moves}
+                </div>
                 <div className="game-info">
-                    <div>{status}</div>
-                    <ol>{moves}</ol>
+                    {status}
                 </div>
             </div>
         );
     }
 }
 
-const cells =
-
 // ========================================
 
 ReactDOM.render(
     <Game/>,
     document.getElementById('root')
-);
-
+)
+;
 
 function calculateWinner(squares) {
     const lines = [
